e252bbaf6b78e49763a34dec97653a177f9b0a44
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-01-13 19:12:40 -0600

    added live chat

--- index.php
+++ index.php
@@ -342,7 +342,19 @@
             duration: 1200,
         })
     </script>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6785b98daf5bfec1dbeb17a9/1ihh5pjpu';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
