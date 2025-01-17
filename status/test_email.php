1d809fe32b3d80cb986d7fea08df3a87cf754645
James <james@lumihost.net>
2025-01-17 17:04:30 -0600

    git rev

--- status/test_email.php
+++ status/test_email.php
@@ -9,10 +9,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Starting test email script...<br>";
-
 function sendTestEmail($toEmail, $toName) {
-    echo "Loading email template...<br>";
     $template = file_get_contents('email_templates/status_alert.html');
     if ($template === false) {
         echo "Failed to load email template.<br>";
@@ -23,7 +20,6 @@ function sendTestEmail($toEmail, $toName) {
 
     $mail = new PHPMailer(true);
     try {
-        echo "Configuring SMTP...<br>";
         // SMTP configuration
         $mail->isSMTP();
         $mail->Host = 'mail.lumihost.net'; // Set the SMTP server to send through
@@ -34,17 +30,14 @@ function sendTestEmail($toEmail, $toName) {
         $mail->Port = 587;
 
         // Recipients
-        echo "Adding recipient: $toEmail...<br>";
         $mail->setFrom('status@lumihost.net', 'Lumi Host Status');
         $mail->addAddress($toEmail, $toName);
 
         // Content
-        echo "Setting email content...<br>";
         $mail->isHTML(true);
         $mail->Subject = 'Test Status Alert';
         $mail->Body = $template;
 
-        echo "Sending email...<br>";
         $mail->send();
         echo "Test email sent successfully to $toEmail!<br>";
     } catch (Exception $e) {
@@ -62,6 +55,4 @@ $testEmails = [
 foreach ($testEmails as $email => $name) {
     sendTestEmail($email, $name);
 }
-
-echo "Test email script completed.<br>";
 ?>
