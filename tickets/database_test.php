114de8619d763876f96f56f4f372842283d30bb3
BeefSnot <161774551+BeefSnot@users.noreply.github.com>
2025-01-16 18:10:33 -0600

    database test error logging

--- tickets/database_test.php
+++ tickets/database_test.php
@@ -1,9 +1,15 @@
 <?php
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Test database connection
-$conn = new mysqli('localhost', 'lumihost_tickets', 'gAhA7C5jzVPQtpTP4CA6', 'lumihost_tickets');
+$conn = new mysqli('localhost', 'lumihost_ticketsystem', 'gAhA7C5jzVPQtpTP4CA6', 'lumihost_ticketsystem');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
     echo "Connected successfully";
 }
 $conn->close();
+?>
