9fd79366253985b68c3158079e09cf59d48b9363
James <james@lumihost.net>
2025-01-17 12:29:04 -0600

    Adding radio page.

---
+++ radio/index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LUMI Radio</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+</head>
+
+<body>
+    <header class="hero page">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img class="img-fluid" src="/assets/img/logonew.png" alt="Lumi Host">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="../index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../about.html">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../apply.html">Apply</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../staff.php">Staff Center</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../radio/index.php">Radio</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section id="radio">
+        <div class="container mt-5">
+            <div class="section-title text-center">
+                <h6>Welcome to</h6>
+                <h4>LUMI Radio<span class="main">.</span></h4>
+            </div>
+            <div class="text-center">
+                <img src="../selfuploads/logo.jpeg" alt="LUMI Radio Logo" class="logo mb-4">
+                <iframe src="https://radio.lumihost.net/public/lumi_radio/embed?theme=dark" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 150px; border: 0;"></iframe>
+                <div class="links mt-4">
+                    <button class="btn btn-primary" onclick="window.open('https://radio.lumihost.net/public/lumi_radio/playlist.pls', '_blank')">
+                        Click to listen with media player
+                    </button>
+                </div>
+                <div class="previous-songs mt-5">
+                    <h2>Previous Songs</h2>
+                    <iframe src="https://radio.lumihost.net/public/lumi_radio/history?theme=dark" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 300px; border: 0;"></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-col py-4">
+                        <h5>Navigation<span class="main">.</span></h5>
+                        <ul>
+                            <li><a href="../index.php">Home</a></li>
+                            <li><a href="../about.html">About</a></li>
+                            <li><a href="../apply.html">Apply</a></li>
+                            <li><a href="../staff.php">Staff Center</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-col py-4">
+                        <h5>Login<span class="main">.</span></h5>
+                        <ul>
+                            <li><a href="https://lumihost.net">Game Panel (coming soon)</a></li>
+                            <li><a href="https://webpanel.lumihost.net:2222">Web Panel</a></li>
+                            <li><a href="https://billing.lumihost.net">Billing Panel</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-col py-4">
+                        <h5>Legal<span class="main">.</span></h5>
+                        <ul>
+                            <li><a href="../tos.php">Terms of Service</a></li>
+                            <li><a href="../pp.php">Privacy Policy</a></li>
+                            <li><a href="../cookies.php">Cookie Policy</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright">
+            <div class="container">
+                <h6 class="mb-0">Copyright © 2025 Lumi Host. | All Rights Reserved |</h6>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="../assets/js/main.js"></script>
+    <script src="https://unpkg.com/aos@2.3.0/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            duration: 1200,
+        });
+    </script>
+</body>
+
+</html>
