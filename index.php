ce295ac98cd444c2f039b60c7f0dc3f25398311f
James <james@lumihost.net>
2025-02-07 20:17:59 -0600

    undoings index.php

--- index.php
+++ index.php
@@ -404,11 +404,10 @@ if (!isset($_SESSION['verified']) || $_SESSION['verified'] !== true) {
             document.querySelector('.cookie-alert').classList.add('show');
         }
     </script>
-    <script src="assets/js/jquery.slim.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.1/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
     <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-    <script src="assets/js/aos.js"></script>
     <script src="assets/js/main.js"></script>
     <script src="https://unpkg.com/aos@2.3.0/dist/aos.js"></script>
     <script>
