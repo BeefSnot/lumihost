13b20a6fbf52268fe6e1bad911256dae0e8843af
James <james@lumihost.net>
2025-02-09 12:49:26 -0600

    test

--- newsletter/manage_users.php
+++ newsletter/manage_users.php
@@ -50,7 +50,7 @@ if ($result === false) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Users</title>
-    <link rel="stylesheet" href="/newsletter/assets/css/newsletter.css">
+    <link rel="stylesheet" href="assets/css/newsletter.css">
 </head>
 <body>
     <header>
